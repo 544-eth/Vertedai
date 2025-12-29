654010e7e2d295cc764823c812dfd36d9d7c0a2a
Austine771 <austine7714@gmail.com>
2025-12-29 19:33:30 +0100

    Initial Commit

---
+++ Verted-AI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Verted-AI
+//
+//  Created by MacBook Pro on 12/29/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
